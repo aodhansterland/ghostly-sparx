e69b154501ee87f2213a613517ebef5c2b476a74
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-09-28 13:49:54 +0100

    Update sparxmeth.js

--- sparxmeth.js
+++ sparxmeth.js
@@ -1,11 +1,1 @@
-<a href="javascript: (() => {   alert('Hello, World!'); })();">
-  Hello, World!
-</a>
-
-javascript: (() => {
-  const allElements = document.querySelectorAll('*');
-
-  for (let element of allElements) {
-    element.style.fontFamily = 'Comic Sans MS';
-  }
-})();
+console.log("Script is running");
