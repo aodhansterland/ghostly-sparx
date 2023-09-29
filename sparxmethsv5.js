fe59965f64ec2fc2753d89c1fc58239673922580
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-09-29 15:01:22 +0100

    Update and rename sparxmethsv4.js to sparxmethsv5.js
    COLORRRR

--- sparxmethsv5.js
+++ sparxmethsv5.js
@@ -1,5 +1,5 @@
 let currentClass = "_MainContent_13pic_40";
-let currentClass2 = "_MainContent_13pic_40";
+let currentClass2 = "_Hello_1ehfm_21";
 let currentClass3 = "_MainContent_13pic_40";
 let currentClass4 = "_MainContent_13pic_40";
 let currentClass5 = "_MainContent_13pic_40";
@@ -14,5 +14,14 @@ if (currentClass !== "") { // check if currentClass is not an empty string
   let elements = document.querySelectorAll(`.${currentClass}`); // select all elements on the page that have the class specified by currentClass variable
   if (elements.length > 0) { // check if there is at least one element with the class
     elements[0].style.backgroundColor = "#121212"; // set the backgroundColor style property of the first element to #0097fd
+  }
+}
+
+
+
+if (currentClass2 !== "") { // check if currentClass is not an empty string
+  let elements = document.querySelectorAll(`.${currentClass2}`); // select all elements on the page that have the class specified by currentClass variable
+  if (elements.length > 0) { // check if there is at least one element with the class
+    elements[0].style.Color = "#7f1e1e"; // set the backgroundColor style property of the first element to #0097fd
   }
 }
