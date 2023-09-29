efa0d9f992da54115c23c2a3f8afb9091e00fb06
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-09-29 14:37:07 +0100

    Update sparxmeth.js

--- sparxmeth.js
+++ sparxmeth.js
@@ -1,1 +1,10 @@
-console.log("Script is running");
+$currentClass = ("BackgroundColor")
+
+
+
+if (currentClass !== "") { // check if currentClass is not an empty string
+  let elements = document.querySelectorAll(`.${currentClass}`); // select all elements on the page that have the class specified by currentClass variable
+  if (elements.length > 0) { // check if there is at least one element with the class
+    elements[0].style.backgroundColor = "#0097fd"; // set the backgroundColor style property of the first element to #0097fd
+  }
+}
